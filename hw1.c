239348e2282047c81a91e69036120bb6f27d84ad
Heojiseok0704 <144936973+Heojiseok0704@users.noreply.github.com>
2023-09-20 02:40:44 +0900

    1주차

---
+++ hw1.c
@@ -0,0 +1,16 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+int main()
+{
+	int result1, result2, result3;
+	int num1, num2;
+	printf("Input two integers : ");
+	scanf("%d %d", &num1, &num2);
+	result1 = (num1 & num2);
+	result2 = (num1 | num2);
+	result3 = (num1 ^ num2);
+		printf("%d & %d = %d\n", num1, num2, result1);
+	printf("%d | %d = %d\n", num1, num2, result2);
+	printf("%d ^ %d = %d", num1, num2, result3);
+	return 0;
+}
