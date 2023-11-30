7f63044557fb45aefcc830fd8907cdd900dee063
Heojiseok0704 <144936973+Heojiseok0704@users.noreply.github.com>
2023-12-01 02:20:52 +0900

    Add files via upload

---
+++ hw9.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+	char str[100];
+	int i = 0;
+
+	printf("Input> ");
+	fgets(str, sizeof(str), stdin);
+
+	while (str[i] != '\0') {
+		if (str[i] >= 'A' && str[i] <= 'Z') {
+			str[i] = str[i] + 32;
+		}
+		else if (str[i] >= 'a' && str[i] <= 'z') {
+			str[i] = str[i] - 32;
+		}
+		i++;
+	}
+	printf("Output> %s\n", str);
+	return 0;
+}
